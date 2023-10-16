7304361dc6a13811875e6ad4c36f178d19fa25c8
Pengl1ng <101352111+pengl1ng@users.noreply.github.com>
2023-10-16 14:41:39 +0300

    pr7-9

--- ИСП.21.1А/Самозванцев, Чуприна/ScientistsApp/Pages/MainPage.xaml.cs
+++ ИСП.21.1А/Самозванцев, Чуприна/ScientistsApp/Pages/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using ScientistsApp.Helpers;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Media;
 using System.Text;
@@ -15,6 +16,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace ScientistsApp.Pages
 {
@@ -171,6 +173,73 @@ namespace ScientistsApp.Pages
                 ws.Cells[3][indexRows] = reps.Scientists.ScFIO;
                 ws.Cells[4][indexRows] = reps.Conferences.ConfName;
                 ws.Cells[5][indexRows] = reps.Conferences.ConfDate;
+            }
+
+            app.Visible = true;
+        }
+
+        private void btnPrintShab_Click(object sender, RoutedEventArgs e)
+        {
+            var app = new Excel.Application();
+
+            Excel.Workbook wb = app.Workbooks.Open($"" + $"{Directory.GetCurrentDirectory()}" + $"\\Шаблон.xlsx");
+            Excel.Worksheet worksheet = (Excel.Worksheet)wb.Worksheets[1];
+
+            int indexRows = 2;
+            worksheet.Cells[1][indexRows] = "Номер";
+            worksheet.Cells[2][indexRows] = "Тема доклада";
+            worksheet.Cells[3][indexRows] = "ФИО докладчика";
+            worksheet.Cells[4][indexRows] = "Конференция";
+            worksheet.Cells[5][indexRows] = "Дата доклада";
+
+            var listReps = context.Reports.ToList();
+
+            foreach (var reps in listReps)
+            {
+                indexRows++;
+                worksheet.Cells[1][indexRows] = indexRows - 2;
+                worksheet.Cells[2][indexRows] = reps.RepTheme;
+                worksheet.Cells[3][indexRows] = reps.Scientists.ScFIO;
+                worksheet.Cells[4][indexRows] = reps.Conferences.ConfName;
+                worksheet.Cells[5][indexRows] = reps.Conferences.ConfDate;
+            }
+            //показать Excel
+            app.Visible = true;
+        }
+
+        private void btnPrintWord_Click(object sender, RoutedEventArgs e)
+        {
+            var app = new Word.Application();
+            Word.Document doc = app.Documents.Add();
+            var listReps = context.Reports.ToList();
+
+            foreach (var reps in listReps)
+            {
+                Word.Paragraph paragraph = doc.Paragraphs.Add();
+                Word.Range range = paragraph.Range;
+                range.Text = $"Тема доклада: {reps.RepTheme}";
+                range.InsertParagraphAfter();
+
+                Word.Paragraph parFIO = doc.Paragraphs.Add();
+                Word.Range rangeFIO = parFIO.Range;
+                rangeFIO.Text = $"ФИО докладчика: {reps.Scientists.ScFIO}";
+                rangeFIO.InsertParagraphAfter();
+
+                Word.Paragraph parConf = doc.Paragraphs.Add();
+                Word.Range rangeConf = parConf.Range;
+                rangeConf.Text = $"Конференция: {reps.Conferences.ConfName}";
+                rangeConf.InsertParagraphAfter();
+
+                Word.Paragraph parDate = doc.Paragraphs.Add();
+                Word.Range rangeDate = parDate.Range;
+                rangeDate.Text = $"Дата: {reps.Conferences.ConfDate.Day}.{reps.Conferences.ConfDate.Month}." +
+                    $"{reps.Conferences.ConfDate.Year}";
+                rangeDate.InsertParagraphAfter();
+
+                if (reps != listReps.LastOrDefault())
+                {
+                    doc.Words.Last.InsertBreak(Word.WdBreakType.wdPageBreak);
+                }
             }
 
             app.Visible = true;
